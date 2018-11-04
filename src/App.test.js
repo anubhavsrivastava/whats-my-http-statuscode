03dc248d6b2d5231e3b6db479e4d98ab76098144
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-04 20:31:11 +0530

    fix: fixed basic app mount test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import App from './containers/appContainer';
+
+const initialState = {
+	application: {
+		version: '0.0.0',
+		name: 'application'
+	}
+};
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const store = mockStore(initialState);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>,
+		div
+	);
+	ReactDOM.unmountComponentAtNode(div);
 });
