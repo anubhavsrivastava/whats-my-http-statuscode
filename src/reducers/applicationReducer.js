de3bdf9f97459b26b17abcc89515b9e1769aa0eb
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-19 17:08:17 +0530

    fixed quotes

--- src/reducers/applicationReducer.js
+++ src/reducers/applicationReducer.js
@@ -1,15 +1,15 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    version: "0.0.0",
-    name: "application"
+	version: '0.0.0',
+	name: 'application'
 };
 
 export default function appReducer(state = initialState, action) {
-    switch(action.type) {
-        case actionTypes.UPDATE_APPLICATION:
-            return Object.assign({}, state, action.payload );
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case actionTypes.UPDATE_APPLICATION:
+			return Object.assign({}, state, action.payload);
+		default:
+			return state;
+	}
 }
