3f8a89f53c107b27285c3e729bbc3ac102e86301
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-21 13:12:08 +0530

    added httpinfo reducer entry

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import applicationReducer from './applicationReducer';
-
+import httpInfoReducer from './httpInfomationReducer';
 
 const rootReducer = combineReducers({
-    application: applicationReducer,
+	application: applicationReducer,
+	httpInfo: httpInfoReducer
 });
 
 export default rootReducer;
