8999f648a5a821cef2eb17eaae58632638c5f5d0
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-21 13:11:38 +0530

    added fetchstabilityData for action

--- src/actions/applicationAction.js
+++ src/actions/applicationAction.js
@@ -1,28 +1,25 @@
 import * as actionTypes from './actionTypes';
 import packageJson from '../../package.json';
+import { fetchStabilityData } from './httpStatusInformationAction';
 
-const updateApplicationInfo = (appInfo) => {
-    return {
-        type: actionTypes.UPDATE_APPLICATION,
-        payload: {
-            version: appInfo.version,
-            name: appInfo.name
-        }
-    };
+const updateApplicationInfo = appInfo => {
+	return {
+		type: actionTypes.UPDATE_APPLICATION,
+		payload: {
+			version: appInfo.version,
+			name: appInfo.name
+		}
+	};
 };
-
 
 const getApplicationInfo = () => {
-    return (dispatch) => {
-
-        return new Promise ((resolve) => {
-            dispatch(updateApplicationInfo(packageJson));
-            resolve(packageJson.name);
-        });
-       
-    };
+	return dispatch => {
+		return new Promise(resolve => {
+			dispatch(updateApplicationInfo(packageJson));
+			dispatch(fetchStabilityData());
+			resolve(packageJson.name);
+		});
+	};
 };
 
-export {
-    getApplicationInfo
-};
+export { getApplicationInfo };
