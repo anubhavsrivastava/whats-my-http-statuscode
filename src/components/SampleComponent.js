d4be15b436cde3febe1ed2daa7f9799b9d5a364e
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-23 23:56:46 +0530

    added blueprint css and sample button entry

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../static/img/logo.svg';
+import { Button, Intent, Spinner } from '@blueprintjs/core';
 
 class SampleComponent extends Component {
 	render() {
@@ -9,7 +10,7 @@ class SampleComponent extends Component {
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Welcome to {name}</h1>
-
+					<Spinner intent={Intent.PRIMARY} />
 					<img src={logo} className="App-logo" alt="logo" />
 				</header>
 			</div>
--- src/containers/appContainer.js
+++ src/containers/appContainer.js
@@ -4,42 +4,45 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import RoutesMapping from '../constant/routes';
 import SampleComponent from '../components/SampleComponent';
-import {getApplicationInfo} from '../actions/applicationAction';
-
+import { getApplicationInfo } from '../actions/applicationAction';
 import '../static/css/App.css';
-
+import 'normalize.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
 const Routes = RoutesMapping.getRoutes();
 
 const mapStateToProps = (state, ownProps) => ({
-    applicationName: state.application.name
+	applicationName: state.application.name
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    updateApplicationInfo: () => {
-        return dispatch(getApplicationInfo());
-    }
+const mapDispatchToProps = dispatch => ({
+	updateApplicationInfo: () => {
+		return dispatch(getApplicationInfo());
+	}
 });
-
 
 class AppContainer extends Component {
+	componentDidMount = () => {
+		this.props.updateApplicationInfo();
+	};
 
-    componentDidMount = () => {
-        this.props.updateApplicationInfo();
-    }
+	render() {
+		const { applicationName } = this.props;
+		return (
+			<React.Fragment>
+				<Switch>
+					{/* <Route path={RoutesMapping.HOME} component={}/> */}
+					<Route exact path={Routes.HTTPSTATUS.path} render={() => <SampleComponent name={applicationName} />} />
+					<Redirect to={Routes.HTTPSTATUS.path} />
+				</Switch>
+			</React.Fragment>
+		);
+	}
+}
 
-    render() {
-        const {applicationName} = this.props;
-        return (
-            <React.Fragment>
-                <Switch>
-                    {/* <Route path={RoutesMapping.HOME} component={}/> */}
-                    <Route exact path={Routes.HTTPSTATUS.path} render={() => <SampleComponent name={applicationName} />} />
-                    <Redirect to={Routes.HTTPSTATUS.path} />
-                </Switch>
-            </React.Fragment>
-        )
-    }
-};
-
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AppContainer));
+export default withRouter(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(AppContainer)
+);
