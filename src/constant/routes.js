c140e6194a969f04314a4ec19ada17150d902d54
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-24 16:53:23 +0530

    added related resource route

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -1,31 +1,25 @@
-
 class RoutesMapping {
-  _routes = [
-    { path: '/', name: 'Home' },
-    { path: '/http-status', exact:true, name: 'httpstatus'},
-  ];
+	_routes = [{ path: '/', name: 'Home' }, { path: '/http-status', exact: true, name: 'httpstatus' }, { path: '/resource', exact: true, name: 'resource' }];
 
-  getRoutes(){
-    const routesMap = {};
-    this._routes.forEach(function(route){
-        let {path, name, exact = false} = route;
-        routesMap[name.toUpperCase()] = { path, name, exact};
-    });
-    return routesMap;
-  }
+	getRoutes() {
+		const routesMap = {};
+		this._routes.forEach(function(route) {
+			let { path, name, exact = false } = route;
+			routesMap[name.toUpperCase()] = { path, name, exact };
+		});
+		return routesMap;
+	}
 
-  /* for future usage */
+	/* for future usage */
 
-  // modifyRoutes(routes){
-  //   this._routes = routes;
-  // }
+	// modifyRoutes(routes){
+	//   this._routes = routes;
+	// }
 
-  // addRoute(route){
-  //   this._routes.push(route);
-  // }
-
+	// addRoute(route){
+	//   this._routes.push(route);
+	// }
 }
-
 
 export default new RoutesMapping();
 export { RoutesMapping };
