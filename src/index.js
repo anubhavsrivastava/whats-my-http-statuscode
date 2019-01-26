e9e31099ec4777539f79f2b732cf1279362a5ac7
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-26 23:48:36 +0530

    removed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import AppContainer from './containers/appContainer';
 import configureStore from './store/configureStore';
 
@@ -20,4 +20,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
