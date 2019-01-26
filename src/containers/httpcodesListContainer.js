0009d9cf9f7e4dc376db7a785a36abfb731bde93
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-27 00:28:50 +0530

    added httplist component

--- src/containers/httpcodesListContainer.js
+++ src/containers/httpcodesListContainer.js
@@ -1,26 +1,80 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Tab, Grid, Segment } from 'semantic-ui-react';
+import { Card, Label, Icon, Tab, Grid, Segment } from 'semantic-ui-react';
 import PageHeader from '../components/layouts/pageHeader';
 
 const mapStateToProps = (state, ownProps) => ({
-	httpCodeClasses: state.httpInfo.httpCodeClasses
+	httpCodeClasses: state.httpInfo.httpCodeClasses,
+	httpCodes: state.httpInfo.httpCodes
 });
 
 const mapDispatchToProps = dispatch => ({});
 
 const IconMap = { '1xx': 'info', '2xx': 'check', '3xx': 'redo', '4xx': 'computer', '5xx': 'server' };
+
+class HTTPCodeList extends Component {
+	componentDidMount = () => {};
+
+	render() {
+		const { httpCodes = {}, currentClassCode = {} } = this.props;
+
+		const currentList = httpCodes[currentClassCode.name] || [];
+		return (
+			<Tab.Pane>
+				<PageHeader title={`${currentClassCode.name} ${currentClassCode.type}`} />
+				<Segment vertical>
+					<Grid container stackable verticalAlign="middle">
+						<Grid.Row>
+							<Grid.Column width={16}>
+								<Card.Group itemsPerRow={2}>
+									{currentList.map(site => {
+										return (
+											<Card key={site.url}>
+												<Card.Content>
+													<Card.Header>
+														<a target="_blank" rel="noopener noreferrer" href={site.url}>
+															{site.repo ? (
+																<Label color="blue" ribbon>
+																	<Icon name="lab" />
+																	Repository
+																</Label>
+															) : (
+																<Label color="orange" ribbon>
+																	<Icon name="at" />
+																	Website
+																</Label>
+															)}
+
+															{site.name}
+														</a>
+													</Card.Header>
+													{/* <Card.Meta>{site.repo ? 'Repository' : 'Website'}</Card.Meta> */}
+													<Card.Description>{site.description}</Card.Description>
+												</Card.Content>
+											</Card>
+										);
+									})}
+								</Card.Group>
+							</Grid.Column>
+						</Grid.Row>
+					</Grid>
+				</Segment>
+			</Tab.Pane>
+		);
+	}
+}
+
 class HTTPCodesListContainer extends Component {
 	componentDidMount = () => {};
 
 	render() {
-		const { httpCodeClasses = [] } = this.props;
+		const { httpCodes = {}, httpCodeClasses = [] } = this.props;
 
 		let panes = httpCodeClasses.map(classCode => {
 			return {
 				menuItem: { key: classCode.name, icon: IconMap[classCode.name], content: `${classCode.name} ${classCode.type}` },
-				render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>
+				render: () => <HTTPCodeList httpCodes={httpCodes} currentClassCode={classCode} />
 			};
 		});
 
