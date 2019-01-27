6289be90c9415c41a35a28e56ce3e27899319330
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-27 13:01:49 +0530

    split pageHeader into pageHeader and contentHeader

---
+++ src/components/common/contentHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Icon, Grid, Header, Segment } from 'semantic-ui-react';
+
+export default function ContentHeader(props) {
+	const { title, description, icon } = props;
+	return (
+		<Segment vertical>
+			<Grid container stackable verticalAlign="middle">
+				<Grid.Row>
+					<Grid.Column width={16}>
+						<Header as="h2">
+							{icon ? <Icon name={icon} /> : null}
+							<Header.Content>
+								{title}
+								<Header.Subheader>{description}</Header.Subheader>
+							</Header.Content>
+						</Header>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</Segment>
+	);
+}
