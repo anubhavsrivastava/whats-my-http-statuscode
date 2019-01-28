efee9dd078e167a691d583ce74192ae84331c5fa
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-01-28 16:07:32 +0530

    moved color and icon mapping to constant file

---
+++ src/constant/params.js
@@ -0,0 +1,2 @@
+export const IconMap = { '1xx': 'info', '2xx': 'check', '3xx': 'redo', '4xx': 'computer', '5xx': 'server' };
+export const ColorMap = { '1xx': 'blue', '2xx': 'green', '3xx': 'brown', '4xx': 'orange', '5xx': 'red' };
