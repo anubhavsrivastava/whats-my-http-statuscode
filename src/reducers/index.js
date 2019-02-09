66577fe3a8e5b29e10bffca67e5bd9bc8705c393
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-02-09 13:47:26 +0530

    removed questionaire reducer (unwanted)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import applicationReducer from './applicationReducer';
 import httpInfoReducer from './httpInfoReducer';
-import questionaireReducer from './questionaireReducer';
 
 const rootReducer = combineReducers({
 	application: applicationReducer,
-	httpInfo: httpInfoReducer,
-	questionaire: questionaireReducer
+	httpInfo: httpInfoReducer
 });
 
 export default rootReducer;
