0bfe991476d9799f9de4b14e5a045fa3650cec81
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-02-11 23:32:57 +0530

    added dropdown for series

--- src/containers/httpQuestionaire/httpStatusCodeAnswerContainer.js
+++ src/containers/httpQuestionaire/httpStatusCodeAnswerContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Divider, Button, Header, Icon, Grid, Segment } from 'semantic-ui-react';
+import { Dropdown, Divider, Button, Header, Icon, Grid, Segment } from 'semantic-ui-react';
 import QuestionComponent from '../../components/flow/questionComponent';
 import HttpClassCardList from '../../components/flow/httpClassCardList';
 const mapStateToProps = (state, ownProps) => ({
 	httpCodes: state.httpInfo.httpCodes,
+	httpCodeSeries: state.httpInfo.httpCodeSeries,
 	httpCodesQuestions: state.httpInfo.questions.httpCodes
 });
 
@@ -17,6 +18,17 @@ class HTTPStatusCodeAnswerContainer extends Component {
 		super(props);
 		this.state = this.initialState;
 	}
+
+	getHttpSeriesDropDown = () => {
+		const { httpCodeSeries = [] } = this.props;
+
+		return httpCodeSeries.map(t => {
+			return {
+				text: `${t.name} - ${t.type}`,
+				value: t.name
+			};
+		});
+	};
 
 	clearState = () => {
 		this.setState(this.initialState);
@@ -34,31 +46,37 @@ class HTTPStatusCodeAnswerContainer extends Component {
 	render() {
 		const { httpCodesQuestions = {}, httpCodes = {}, currentSeries } = this.props;
 		const { currentIndex, answered, currentAnswer } = this.state;
-		const currentQuestion = httpCodesQuestions.find(t => t.id === currentIndex);
+		const currentQuestion = {};
+		this.getHttpSeriesDropDown();
+		// const currentQuestion = httpCodesQuestions.find(t => t.id === currentIndex);
 		return (
 			<React.Fragment>
 				<Segment vertical>
 					<Grid className="container-box" container stackable verticalAlign="middle">
 						<Grid.Row>
 							<Grid.Column width={16}>
-								{answered ? (
-									<Segment placeholder>
-										<Grid.Row>
-											<Header as="h2">
-												<Icon name="flag checkered" />
-												<Header.Content>HTTP Status Code Series </Header.Content>
-											</Header>
-											<HttpClassCardList httpClassList={httpCodes['2xx'].filter(t => currentAnswer.includes(t.name))} />
-										</Grid.Row>
-										<Divider />
-										<Grid.Row>
-											<Button floated="right" icon="redo" color="black" content="Try Again" onClick={this.clearState} />
-										</Grid.Row>
-									</Segment>
+								{currentSeries ? (
+									answered ? (
+										<Segment placeholder>
+											<Grid.Row>
+												<Header as="h2">
+													<Icon name="flag checkered" />
+													<Header.Content>HTTP Status Code Series </Header.Content>
+												</Header>
+												<HttpClassCardList httpClassList={httpCodes['2xx'].filter(t => currentAnswer.includes(t.name))} />
+											</Grid.Row>
+											<Divider />
+											<Grid.Row>
+												<Button floated="right" icon="redo" color="black" content="Try Again" onClick={this.clearState} />
+											</Grid.Row>
+										</Segment>
+									) : (
+										<Segment placeholder>
+											<QuestionComponent onOptionChoose={this.onOptionChoose.bind(this, currentQuestion)} question={currentQuestion} />
+										</Segment>
+									)
 								) : (
-									<Segment placeholder>
-										<QuestionComponent onOptionChoose={this.onOptionChoose.bind(this, currentQuestion)} question={currentQuestion} />
-									</Segment>
+									<Dropdown placeholder="Select Series" fluid selection options={this.getHttpSeriesDropDown()} />
 								)}
 							</Grid.Column>
 						</Grid.Row>
