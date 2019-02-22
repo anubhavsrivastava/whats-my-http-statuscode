55c861f49332e95fd728f47eae228100cfd632b9
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-02-23 00:45:58 +0530

    made more link optional in card

--- src/components/flow/httpCodeInfoCard.js
+++ src/components/flow/httpCodeInfoCard.js
@@ -2,17 +2,27 @@ import React from 'react';
 
 import { Label, Icon, Card } from 'semantic-ui-react';
 
+const getContent = (color, icon, httpCode) => {
+	return (
+		<Label color={color} ribbon>
+			<Icon name={icon} />
+			{httpCode.msg}
+		</Label>
+	);
+};
+
 export default function HttpCodeInfoCard({ httpCode, color, icon }) {
 	return (
 		<Card key={httpCode.code}>
 			<Card.Content>
 				<Card.Header>
-					<a target="_blank" rel="noopener noreferrer" href={httpCode.more}>
-						<Label color={color} ribbon>
-							<Icon name={icon} />
-							{httpCode.msg}
-						</Label>
-					</a>
+					{httpCode.more ? (
+						<a target="_blank" rel="noopener noreferrer" href={httpCode.more}>
+							{getContent(color, icon, httpCode)}
+						</a>
+					) : (
+						getContent(color, icon, httpCode)
+					)}
 				</Card.Header>
 
 				<Card.Description>
