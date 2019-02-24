27aa45d11a2bd492267d34c2062756e51da240dd
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-02-24 20:06:19 +0530

    removed unwanted action/reducer

--- src/actions/applicationAction.js
+++ src/actions/applicationAction.js
@@ -1,21 +1,9 @@
-import * as actionTypes from './actionTypes';
 import packageJson from '../../package.json';
 import { fetchhttpCodeSeriesQuestions, fetchHttpCodesQuestions, fetchHttpCodesData, fetchStabilityData, fetchRelatedSitesData, fetchhttpCodeSeriesData } from './httpInfoAction';
-
-const updateApplicationInfo = appInfo => {
-	return {
-		type: actionTypes.UPDATE_APPLICATION,
-		payload: {
-			version: appInfo.version,
-			name: appInfo.name
-		}
-	};
-};
 
 const getApplicationInfo = () => {
 	return dispatch => {
 		return new Promise(resolve => {
-			dispatch(updateApplicationInfo(packageJson));
 			dispatch(fetchStabilityData());
 			dispatch(fetchRelatedSitesData());
 			dispatch(fetchhttpCodeSeriesData());
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import applicationReducer from './applicationReducer';
 import httpInfoReducer from './httpInfoReducer';
 
 const rootReducer = combineReducers({
-	application: applicationReducer,
 	httpInfo: httpInfoReducer
 });
 
