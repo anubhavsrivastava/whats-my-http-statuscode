e8a840d887d6a572f098454725cdaae62e51cad8
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-03-01 23:33:05 +0530

    removed unwanted code

--- src/components/layouts/defaultLayout.js
+++ src/components/layouts/defaultLayout.js
@@ -45,41 +45,6 @@ const FixedMenuLayout = props => (
 		<footer style={{ height: footerHeight }}>
 			<Segment inverted vertical>
 				<Container textAlign="center">
-					{/* <Grid divided inverted stackable>
-						<Grid.Column width={3}>
-							<Header inverted as="h4" content="Group 1" />
-							<List link inverted>
-								<List.Item as="a">Link One</List.Item>
-								<List.Item as="a">Link Two</List.Item>
-								<List.Item as="a">Link Three</List.Item>
-								<List.Item as="a">Link Four</List.Item>
-							</List>
-						</Grid.Column>
-						<Grid.Column width={3}>
-							<Header inverted as="h4" content="Group 2" />
-							<List link inverted>
-								<List.Item as="a">Link One</List.Item>
-								<List.Item as="a">Link Two</List.Item>
-								<List.Item as="a">Link Three</List.Item>
-								<List.Item as="a">Link Four</List.Item>
-							</List>
-						</Grid.Column>
-						<Grid.Column width={3}>
-							<Header inverted as="h4" content="Group 3" />
-							<List link inverted>
-								<List.Item as="a">Link One</List.Item>
-								<List.Item as="a">Link Two</List.Item>
-								<List.Item as="a">Link Three</List.Item>
-								<List.Item as="a">Link Four</List.Item>
-							</List>
-						</Grid.Column>
-						<Grid.Column width={7}>
-							<Header inverted as="h4" content="Footer Header" />
-							<p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-						</Grid.Column>
-					</Grid> */}
-
-					{/* <Divider inverted section /> */}
 					{/* <Image centered size="mini" src="/logo.png" /> */}
 					<List horizontal inverted divided link size="small">
 						<List.Item as="a" href="#">
