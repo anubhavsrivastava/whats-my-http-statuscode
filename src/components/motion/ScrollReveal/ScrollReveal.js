c8e57aed1d78972bef44c9e0299abc7e8e6d42d3
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-12 17:04:39 +0530

    added scrollreveal package

---
+++ src/components/motion/ScrollReveal/ScrollReveal.js
@@ -0,0 +1,5 @@
+// It doesn't work well if there are multiple instances of ScrollReveal,
+// so we have to create a module returning an instance:
+// file ScrollReveal.js:
+import ScrollReveal from 'scrollreveal';
+export default ScrollReveal();
